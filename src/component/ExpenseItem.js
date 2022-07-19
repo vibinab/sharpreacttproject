faaadf4f4e5cf4b9b1787a876567929306975d9b
vibinab <abvibin4@gmail.com>
2022-07-19 10:55:28 +0530

    first commit

---
+++ src/component/ExpenseItem.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function ExpenseItem() {
+  return (
+    <div>
+        <h3>Expense items</h3>
+        <p>food Rs 10</p>
+        <p>Petrol Rs 100</p>
+        <p>Movies Rs 200</p>
+    </div>
+  )
+}
