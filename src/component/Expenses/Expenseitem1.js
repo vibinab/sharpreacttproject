48eedbc7632b27a582ccb177abc1cbc89e8e5c7b
vibinab <abvibin4@gmail.com>
2022-07-20 14:07:39 +0530

    changeamount

--- src/component/Expenses/Expenseitem1.js
+++ src/component/Expenses/Expenseitem1.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React ,{useState}from 'react'
 
 import ExpenseDate from './ExpenseDate'
 import ExpenseDetail from './ExpenseDetail'
@@ -8,32 +8,49 @@ import Card from '../UI/Card'
 
 export default function Expenseitem1(props) {
 
-    let date=props.date
-
-    let title=props.title
-
-    let amount=props.amount
-
-    const deletehandler=()=> {
-      console.log(title)
-      console.log("del")
-      title="" 
-      amount=""
-      date=""
-      console.log(title)
+    const [amount,setamount]=useState(props.amount)
+    
+    const changeamount=()=>{
+      setamount(100)
     }
+
+    // let date=props.date
+
+    // let title=props.title
+
+    // let amount=props.amount
+
+    // const deletehandler=()=> {
+    //   console.log(title)
+    //   console.log("del")
+    //   title="" 
+    //   amount=""
+    //   date=""
+    //   console.log(title)
+    // }
+    // const [title,settitle]= useState(props.title)
+
+    
+    // const titleedit= ()=> {
+    //    settitle("updated")
+    // }
+
   
     return (
     <div className='expense-item'>
-         <ExpenseDate date={date}/>
+         <ExpenseDate date={props.date}/>
          
          <div className='expense-item__description'>
             {/* <h2>{props.title}</h2> */}
             {/* <h2>{props.location}</h2> */}
-            <ExpenseDetail childtitle={title} childamount={amount}/>
+            <ExpenseDetail childtitle={props.title} childamount={amount}/>
             
             {/* <div className='expense-item__price'>{props.amount}</div> */}
-            <button className="expensedelete" onClick={deletehandler}>Delete Expense</button>
+            {/* <button className="expensedelete" onClick={deletehandler}>Delete Expense</button> */}
+
+            {/* <button onClick={titleedit}>edit title</button> */}
+            <button onClick={changeamount}>changeamount</button>
+            
          </div>
     
     </div>
