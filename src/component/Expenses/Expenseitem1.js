94c4e0761b93cf9b6db50abd4fcb85ce27221058
vibinab <abvibin4@gmail.com>
2022-07-20 11:04:25 +0530

    delete expense

--- src/component/Expenses/Expenseitem1.js
+++ src/component/Expenses/Expenseitem1.js
@@ -8,18 +8,29 @@ import Card from '../UI/Card'
 
 export default function Expenseitem1(props) {
 
+    let date=props.date
+
+    let title=props.title
+
+    let amount=props.amount
+
     const deletehandler=()=> {
+      console.log(title)
       console.log("del")
+      title="" 
+      amount=""
+      date=""
+      console.log(title)
     }
   
     return (
     <div className='expense-item'>
-         <ExpenseDate date={props.date}/>
+         <ExpenseDate date={date}/>
          
          <div className='expense-item__description'>
             {/* <h2>{props.title}</h2> */}
             {/* <h2>{props.location}</h2> */}
-            <ExpenseDetail childtitle={props.title} childamount={props.amount}/>
+            <ExpenseDetail childtitle={title} childamount={amount}/>
             
             {/* <div className='expense-item__price'>{props.amount}</div> */}
             <button className="expensedelete" onClick={deletehandler}>Delete Expense</button>
